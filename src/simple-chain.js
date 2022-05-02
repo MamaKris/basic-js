9608c3b7310c30573a0a0381b0ac0d582d20cdba
Kristina <93838200+MamaKris@users.noreply.github.com>
2022-05-02 15:31:06 +0200

    Update simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,28 +4,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-};
+let chainMaker = {
+    'result': [],
+    'addLink': function(num){
+        chainMaker.result.push(`( ${num} )`);
+        return chainMaker;
+    },
+    'removeLink': function(num){
+        if ((typeof num != 'number') || ((num % 1) != 0) || (num > chainMaker.result.length) || (num < 0)){
+            chainMaker.result = [];
+            throw new Error("You can't remove incorrect link!.");
+        }
+        chainMaker.result.splice(num - 1, 1);
+        
+        return chainMaker;
+    },
+    'reverseChain': function(){
+        chainMaker.result.reverse();
+        return chainMaker;
+    },
+    'getLength': function(){
+        return chainMaker.result.length;
+    },
+    'finishChain': function(){
+        let result1 = chainMaker.result.join('~~');
+        chainMaker.result = [];
+        return result1;
+    },
+    
+}
 
 module.exports = {
   chainMaker
